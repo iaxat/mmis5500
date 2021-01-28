c8bec545c202a099a21c97b3d69b0ab9ac0db602
Kumar Akshat <55215422+iaxat@users.noreply.github.com>
2021-01-28 12:44:57 -0700

    up

--- week2/class_private_data_sec_10.5.py
+++ week2/class_private_data_sec_10.5.py
@@ -18,7 +18,7 @@ class Person():
 
 # __ double underscore is used to make data private
 # need of data privatization is to prevent errors saving it from hackers is not the reason
-
+# Python interpreter handles calling init function because its private that is why we do not need to call it specifically, it is the first that initializes when the class is called
 
     def calc_avg_grades(self):
         return np.mean(self.grades)
