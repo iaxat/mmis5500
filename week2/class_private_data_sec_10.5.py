15a7a0eb98c34c22f672163b12eb6b514e4bf422
Kumar Akshat <55215422+iaxat@users.noreply.github.com>
2021-01-28 12:38:28 -0700

    up

--- week2/class_private_data_sec_10.5.py
+++ week2/class_private_data_sec_10.5.py
@@ -1,3 +1,13 @@
 # Section 10.5
 # making data private = encapsulation
 
+class Person():
+    def __init__(self,name,age,height,grades) -> None:
+        pass
+        self.name = name
+        self.age = age
+        self.height = height
+        self.grades = grades
+
+    def calc_avg_grades(self):
+        
