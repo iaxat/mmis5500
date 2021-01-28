7847243aa0505304d9826c0147c3ac66e25f62f3
Kumar Akshat <55215422+iaxat@users.noreply.github.com>
2021-01-28 11:51:15 -0700

    up

---
+++ week2/classes_sec_10.2.py
@@ -0,0 +1,11 @@
+# From the course MIS 5500
+# Advance Python
+# By Andrew Brim
+
+# Section 10.2 
+# Classes and Objects
+# Book: Intro to Computer Science
+
+# This program covers difference between Dict and Classes
+
+person_dict = {}
