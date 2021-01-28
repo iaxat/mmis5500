9dde6d20edc8983726e318576ae393531debaee0
Kumar Akshat <55215422+iaxat@users.noreply.github.com>
2021-01-28 12:41:40 -0700

    up

--- week2/class_private_data_sec_10.5.py
+++ week2/class_private_data_sec_10.5.py
@@ -3,13 +3,19 @@
 
 import numpy as np
 
+
 class Person():
-    def __init__(self,name,age,height,grades) -> None:
+
+# __init__ is a constructor also known as initializer
+    def __init__(self,name,age,height,grades,address) -> None:
         pass
         self.name = name
         self.age = age
         self.height = height
         self.grades = grades
+        self.__address = address
+# __ dpuble uncderscore is used to make data private
+
 
     def calc_avg_grades(self):
         return np.mean(self.grades)
