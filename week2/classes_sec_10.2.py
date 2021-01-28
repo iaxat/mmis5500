bc9445dd379ed23fca5f53a27200690cf4d08b53
Kumar Akshat <55215422+iaxat@users.noreply.github.com>
2021-01-28 12:06:51 -0700

    up

--- week2/classes_sec_10.2.py
+++ week2/classes_sec_10.2.py
@@ -8,6 +8,9 @@
 
 # This program covers difference between Dict and Classes
 
+from typing import AsyncGenerator
+
+
 person_dict = {}
 person_dict['name'] = 'axat'
 person_dict['age'] = 25
@@ -15,4 +18,19 @@ person_dict['height'] = 59
 person_dict['grades'] = [98,96,102]
 
 print(person_dict)
+
+class Person():
+    def __init__(self,name,age,height,grades) -> None:
+        pass
+        self.name = name
+        self.age = age
+        self.height = height
+        self.grades = grades
+
+p1 = Person('axat',25,59,[98,96,102])
+print(p1)
+print('')
+print(p1.age)
+print(p1.grades)
+print(p1.name)
 
