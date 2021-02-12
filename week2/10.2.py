32c91b01ae6e050de9c0f49aa625f0fb2bb3aadc
iaxat <55215422+iaxat@users.noreply.github.com>
2021-02-12 03:19:24 -0700

    up

--- week2/10.2.py
+++ week2/10.2.py
@@ -1,6 +1,8 @@
 # 10.2
 # Advance Python
 # Class and Objects
+import numpy as np
+
 
 person_dct = {}
 person_dct['name'] = 'andy'
@@ -20,6 +22,17 @@ class Person():
         self.grades = grades
     
 
+    # avg grades
+    def cal_avg_grades(self):
+        return np.mean(self.grades)
+
 p1 = Person('andy',39,75,[85,89,93])
 
 print(p1)
+
+print('age: ',p1.age)
+print('name: ',p1.name)
+print('height: ',p1.height)
+print('grades: ',p1.grades)
+print('avg_grades: ',p1.cal_avg_grades())
+# calling function from a object would need parentheses
