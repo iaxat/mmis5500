bc7330da41a90abd5d55f8b06f2aa5f3cb0b81c9
iaxat <55215422+iaxat@users.noreply.github.com>
2021-02-11 17:50:18 -0700

    up

---
+++ assignment/hw1/DeckOfCards.py
@@ -0,0 +1,23 @@
+import random
+
+class Card():
+    def __init__(self, suit, face):
+        self.suit = suit
+        self.face = face
+        
+class DeckOfCards():
+    def __init__(self):
+        self.deck = []
+        suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
+        faces = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
+        
+        for suit in suits:
+            for face in faces:
+                self.deck.append(Card(suit, face))
+                
+    def shuffle_deck(self):
+        random.shuffle(self.deck)
+        
+    def print_deck(self):
+        for card in self.deck:
+            print(card.face, "of", card.suit, end=", ")
