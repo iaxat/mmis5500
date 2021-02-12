56da811aef6452779b412f54bd9e87dc700fd4fd
iaxat <55215422+iaxat@users.noreply.github.com>
2021-02-12 03:05:34 -0700

    up

--- week2/10.2.py
+++ week2/10.2.py
@@ -9,13 +9,17 @@ person_dct['height'] = 75
 person_dct['grades'] = [85,89,93]
 
 print(person_dct)
+# dict
 
-
-
+# class
 class Person():
     def __init__(self, name, age, height, grades) -> None:
         self.name = name
         self.age = age
         self.height = height
         self.grades = grades
-        
+    
+
+p1 = Person('andy',39,75,[85,89,93])
+
+print(p1)
