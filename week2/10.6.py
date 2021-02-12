2b4ac76fa1aa78d3c190ab18d5948fe54e199802
iaxat <55215422+iaxat@users.noreply.github.com>
2021-02-12 15:32:30 -0700

    up

--- week2/10.6.py
+++ week2/10.6.py
@@ -1,2 +1,22 @@
 # Section 10.6
 
+import random
+
+class Card():
+    def __init__(self, suit, face) -> None:
+        self.suit = suit
+        self.face = face
+
+    
+class DeckOfCards():
+    def __init__(self, deck) -> None:
+        self.deck = deck
+
+    def shuffle_deck(self):
+        random.shuffle(self.deck)
+
+    def print_deck(self):
+        for card in self.deck:
+            print(card.face,'of',card.suit)
+
+
