aa11cf65604552ce4736e5fc60ce129a6b9a49ff
iaxat <55215422+iaxat@users.noreply.github.com>
2021-02-12 02:58:44 -0700

    up

--- week2/10.2.py
+++ week2/10.2.py
@@ -1,11 +1,13 @@
 # 10.2
 # Advance Python
-
+# Class and Objects
 
 person = {}
 person['name'] = 'andy'
 person['age'] = 39
 person['height'] = 75
 person['grades'] = [85,89,93]
+
+
 
 print(person)
