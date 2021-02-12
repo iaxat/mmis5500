fe0b05c9c3d4e057b0c933076f871962ab1d76be
iaxat <55215422+iaxat@users.noreply.github.com>
2021-02-12 07:16:06 -0700

    up

--- week2/10.5.py
+++ week2/10.5.py
@@ -3,3 +3,15 @@
 # Class Private Date
 # Simulating Private Attributes
 
+class Person():
+    def __init__(self, name, age, height, grades) -> None:
+        self.name = name
+        self.age = age
+        self.height = height
+        self.grades = grades
+    
+
+    # avg grades
+    def cal_avg_grades(self):
+        return np.mean(self.grades)
+    # Setting getters and setters for this class
