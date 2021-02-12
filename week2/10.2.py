8ab48339d2705a86efd12c5c41f1fdfd2497207f
iaxat <55215422+iaxat@users.noreply.github.com>
2021-02-11 22:10:36 -0700

    up

--- week2/10.2.py
+++ week2/10.2.py
@@ -1,6 +1,8 @@
 # Section 10.1 and 10.2
 # Python MMIS 5500
 # Classes and Objects
+# Dict only handles static typed data
+# Dict can not handle fundtions inside them
 
 person = {}
 person['name'] = 'andy'
