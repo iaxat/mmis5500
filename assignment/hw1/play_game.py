e3ece3859828c0a5cf401c59adf477da5ec705e7
iaxat <55215422+iaxat@users.noreply.github.com>
2021-02-12 18:42:15 -0700

    up

--- assignment/hw1/play_game.py
+++ assignment/hw1/play_game.py
@@ -1,35 +1,42 @@
-# Game function
-# HW 1
-#  Python MIS5500
-# Blackjack
 
-from assignment.hw1.DeckOfCards import DeckOfCards
+
 import os
 import sys
-__import__ (DeckOfCards)
 
 
 
+
+# Class to play game
 class PlayGame():
     def __init__(self):
         self.d = DeckOfCards()
 
+    def deal_cards(self):
+        print('Dealing Cards to User')
+        self.user_score = 0
+        self.dealer_score = 0
+        self.user_cards = []
 
-    # def deal_cards(self):
-
+    
 
     def play_game(self):
-        print('Starting the game of BlackJack (21)')
-        print('Kindly do not waste all your money :-P')
         print('')
-        print('Initial Deck without Shuffle')
+        print('')
+        print('\t\tStarting the game of BlackJack (21)')
+        print('\t\tKindly do not waste all your money :-P')
+        print('')
+        print('\t\t\t\tInitial Deck without Shuffle')
         self.d.print_deck()
-        print('----------------------------------------')
-        print('Deck to be shuffled')
-
-
-
-
+        print('')
+        print('\t\t----------------------------------------')
+        print('\t\t\t\tDeck to be shuffled')
+        self.d.shuffle_deck()
+        print('')
+        print('\t\t\t\tDeck Shuffled')
+        print('\t\t----------------------------------------')
+        self.d.print_deck()
+        print('')
+        print(self.d.get_deck_array())
 
 play = PlayGame()
-print(play.play_game())
+play.play_game()
